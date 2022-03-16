e1a3e31b201e04699e887e7236b2df2e1af3461c
autobitacora <autobitacora@gmail.com>
2022-03-16 16:53:58 -0300

    add random forest model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from telnetlib import STATUS
+from turtle import st
 import joblib
 import re
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 import sys
 sys.path.append("..")
 
@@ -10,27 +12,75 @@ app = FastAPI()
 
 model = joblib.load('models/modelo_lg.mod')
 
-models = {'logistic_regression':'lg','naive-bayes':'nb', 'neural':'nn', 'random-forest':'rf', 'adaboost':'ab'}
+models = {'lg':'logistic-regression','nb': 'naive-bayes', 'nn':'neural-network', 'rf':'random-forest', 'ab':'adaboost', 'xg': 'XGBoost'}
+
+campos = ["algun_contenido_publicado.",
+		"comentario_creado",
+		"curso_visto",
+		"entrega_creada.",
+		"finalización_actividad_curso",
+		"formulario_entrega_visto.",
+		"comenzado_intento",
+		"intento_cuestionario_visualizado",
+		"intento_cuestionario_revisado",
+		"ntento_enviado",
+		"mensaje_creado",
+		"modulo_curso_visto",
+		"perfil_usuario_visto",
+		"resumen_intento_cuestionario_visualizado",
+		"enviado_entrega",
+		"suscrito_discusión",
+		"visualizado_estado_entrega.",
+		"tema_creado",
+		"tema_visto",
+		"fichero_subido",
+		"usuario_calificado",
+		"lista_usuarios_vista",
+		"mensaje_borrado",
+		"borrado_suscripcion_discusion",
+		"tema_borrado",
+		"informe_notas_usuario_visto",
+		"mensaje_actualizado",
+		"instancia_módulo_curso_visualizada",
+		"informe_resumen_notas_visto",
+		"lista_insignias_vista",
+		"envío_actualizado",
+		"usuario_matriculado_curso",
+		"elemento_calificacion_actualizado",
+		"informe_usuario_visualizado",
+		"informe_usuario_curso_visto",
+		"suscripcion_activada",
+		"curso_actualizado",
+		"elemento_calificacio_creado",
+		"completo"]
 
 @app.get('/')
 def get_root():
+	""" Presentacion de la API
 
-	return {'message': 'Welcome to the unab data science test api'}
+	Returns:
+		dict : mensaje
+	"""
+	return {'mensaje': 'Welcome to the unab data science test api'}
 
 
 @app.get('/models')
 async def show_models():
+	""" Mostramos los modelos que tenemos configurados
 
+	Returns:
+		dict : lista de modelos
+	"""
 	return {'modelos':models}
 
 
 @app.get('/metric/{model}')
-async def show_metric():
+async def show_metric(model: str):
 
-	return {'modelos':models}
+	return {'modelo elegido':model}
 
 
-@app.get('/predict/{model}/query')
-async def show_predict():
+@app.post('/predict/{model}')
+async def show_predict(model: str, algun_contenido_publicado: str = ''):
 
-	return {'modelos':models}
+	return {'modelo elegido':model, 'algun_contenido_publicado': algun_contenido_publicado}
